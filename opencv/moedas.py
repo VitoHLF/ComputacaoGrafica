1ce3780e2949dbacfcb2ca1a91609e79af6e2e1a
VICTOR HUGO LOPES FERREIRA <0040962213025@americana.fatec.br>
2024-03-27 20:12:04 -0300

    240327

---
+++ opencv/moedas.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2 as cv
+
+#ISSO É UM EXEMPLO
+
+""" cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting...")
+        break
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('frame',gray)
+    
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows() """
+
+100 - 7
+50 - 5
+25 - 5
+10 - 8
+5 - 8
+1 - 8
+
+print(100*7 + 50*5 + 25*5 + 10*8 + 5*8 + 1*8)
+    
